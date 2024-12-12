c382b23be6bd0c3417c95ba973bb7b60da874ed3
lecopivo <skrivantomas@seznam.cz>
2024-12-12 05:11:50 -0500

    update examples

--- examples/dense.lean
+++ examples/dense.lean
@@ -5,6 +5,7 @@ open Eigen
 def main : IO Unit := do
   let A : Matrix 2 2 := ⟨FloatArray.mk #[2,1,1,2], by native_decide⟩
   let b : Matrix 2 1 := ⟨FloatArray.mk #[1,1], by native_decide⟩
-  IO.println A
-  IO.println b
-  IO.println (A.ldlt.solve b)
+
+  IO.println s!"A  = {A}"
+  IO.println s!"b  = {b}"
+  IO.println s!"A⁻¹ = {A.ldlt.solve b}"
--- examples/sparse.lean
+++ examples/sparse.lean
@@ -3,7 +3,10 @@ import Eigen
 open Eigen
 
 def main : IO Unit := do
-  let entries : Array (Triplet 2 2) := (#[(0,0,1.0), (1,1,-1.0), (0,1, 2.0)] : Array (Nat×Nat×Float))
-  let B := SparseMatrix.mk entries
-  IO.println B.toDense
+  let entries : Array (Triplet 2 2) := (#[(0,0,2.0), (1,0,1.0), (1,1,2.0), (0,1, 1.0)] : Array (Nat×Nat×Float))
+  let A := SparseMatrix.mk entries
+  let b : Matrix 2 1 := ⟨FloatArray.mk #[1,1], by native_decide⟩
 
+  IO.println s!"A  = {A.toDense}"
+  IO.println s!"b  = {b}"
+  IO.println s!"A⁻¹ = {A.simplicialLLT.solve b}"
